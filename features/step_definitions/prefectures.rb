7d20a66a655bf71d9a0ab81fde8dc3ba760f39d5
ichy <ichylinux@gmail.com>
2015-06-25 21:20:39 +0900

    源泉徴収税

--- features/step_definitions/prefectures.rb
+++ features/step_definitions/prefectures.rb
@@ -1,12 +1,19 @@
-もし /^JISで定義されているコードは以下の通りです。$/ do |ast_table|
-  assert rows = ast_table.raw
-  assert_equal 47, rows.size
+もし /^JISで定義されているコードは次の通りです。$/ do |ast_table|
+  count = 0
 
-  rows.each do |row|
-    code = row[0]
-    name = row[1]
-    assert p = TaxJp::Prefecture.find_by_code(code)
-    assert_equal name, p.name
+  ast_table.raw.each do |row|
+    5.times do |i|
+      code = row[i*2]
+      name = row[i*2+1]
+      next if code.empty?
+      
+      assert p = TaxJp::Prefecture.find_by_code(code)
+      assert_equal name, p.name
+      
+      count += 1
+    end
   end
+
+  assert_equal 47, count
 end
 
