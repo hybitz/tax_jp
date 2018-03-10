5f0830ed1a9a2e969b54f4bcb92f3c973c59b421
ichy <ichylinux@gmail.com>
2018-03-10 14:14:55 +0900

    雇用保険の事業区分

--- lib/tax_jp/const.rb
+++ lib/tax_jp/const.rb
@@ -1,5 +1,12 @@
 module TaxJp
 
+  # 雇用保険の事業区分
+  EMPLOYMENT_INSURANCE_TYPES = {
+    EMPLOYMENT_INSURANCE_TYPE_GENERAL = 1 => '一般',
+    EMPLOYMENT_INSURANCE_TYPE_AGRIC = 2 => '農林水産・清酒製造',
+    EMPLOYMENT_INSURANCE_TYPE_CONST = 3 => '建設'
+  }
+
   # 消費税区分
   TAX_TYPES = {
     TAX_TYPE_NONTAXABLE = 1 => '非課税',
